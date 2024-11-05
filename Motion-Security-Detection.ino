c31669ca06b460c197f75ab1a554aa85ce9d512b
ryanliupie <ryanliu314@gmail.com>
2024-11-05 14:41:00 -0500

    Create variables for MP3 connection

--- Motion-Security-Detection.ino
+++ Motion-Security-Detection.ino
@@ -1,12 +1,28 @@
+#include <SoftwareSerial.h> //Allows another serial port with different pin to allow both computer and MP3 to communicate simultaneously
+
+#include <DFRobotDFPlayerMini.h> //This allows any MP3 player to work and so you can manage them
+
+
+
 int pirPin = 12; //This is the middle pin on the PIR to actually detect that motion and we must set it 
+int txPin = 2; // pin used to sent data out to MP3 player from arduino 
+int rxPin = 3; // pin recieves information from MP3 player with software serial
+
+SoftwareSerial efxSerial (txPin, rxPin); //We need to make a name for the software serial in relation to project; in this case, efx = sound effects which we use rx and tx pins 
+DFRobotDFPlayerMini efxPlayer; 
 
 int motionStatus = 0; //This is to track the motion of the sensor
 int pirState = LOW; //this tracks the state change so UNO wont print repeatedly 
 
 void setup() {
  pinMode(pirPin, INPUT); //so this sets the PIR sensor into an input
- Serial.begin(9600); //Establishes communication between arduino and device (PIR sensor) 9600 bits p/s
- delay(1);           //Allows the input to stabilize
+ pinMode(rxPin, INPUT); //Since we are recieving data we are in a sense "inputting" data in our system
+ pinMode(txPin, OUTPUT); //Since we are transmitting data, it is getting sent out
+ Serial.begin(9600); //Establishes communication between arduino and device (PIR sensor) 9600 bits p/s (common baud rate for serial connection)
+ efxSerial.begin(9600) //Establishes communication between arduino and MP3 player 
+ efxPlayer.begin(efxSerial); //we tell the efxPlayer to use efxSerial 
+ efxPlayer.volume(20) // We can set the volume of the MP3 player to whatever; this case it is 20 
+ delay(3000);           //Allows the input to stabilize or calibrate (sensor)
 }
 
 void loop() {
@@ -30,6 +46,3 @@ else {
     }
    }
   } 
-
-//test git commit 
-// test commit 2 
